ab231b491c932f85397e56e3afdbc268d192a7ae
Rafal Enden <git@enden.com>
2019-05-13 15:50:23 +0100

    Add a11y addon to Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,9 @@
-import { configure, addParameters } from '@storybook/react'
+import { configure } from '@storybook/react'
 
 const req = require.context('../src/components', true, /.*\.stories\.(js|jsx)$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
-
-addParameters({
-  options: {
-    showAddonPanel: false
-  }
-})
 
 configure(loadStories, module)
--- src/components/ActivityFeed.stories.jsx
+++ src/components/ActivityFeed.stories.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
+import { storiesOf, addDecorator } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
 
 import ActivityFeed from './ActivityFeed'
 import activityFeedFixtures from '../../fixtures/activity_feed/'
+
+addDecorator(withA11y)
 
 storiesOf('ActivityFeed', module)
   .add('entire feed', () => <ActivityFeed activities={activityFeedFixtures} />)
