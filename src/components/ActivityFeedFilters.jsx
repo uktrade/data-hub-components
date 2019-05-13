9dce939c111097123df607225fd45a8d02f98e8f
Rafal Enden <git@enden.com>
2019-05-13 11:37:08 +0100

    Use <Details> component in ActivityFeedFilters

--- src/components/ActivityFeedFilters.jsx
+++ src/components/ActivityFeedFilters.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
   Checkbox,
+  Details,
   Select,
   Label,
   Link,
@@ -11,23 +12,26 @@ import {
 export default class ActivityFeedFilters extends React.Component {
 	render() {
     return (
-      <form>
-        <GridRow style={{ display: 'none' }}>
-          <GridCol>Show me</GridCol>
-          <GridCol>
-            <Checkbox>My activity</Checkbox>
-          </GridCol>
-          <GridCol>
-            <Label>Activity types</Label>
-            <Select>
-              <option>All activity</option>
-            </Select>
-          </GridCol>
-          <GridCol>
-            <Link href="#">All filters</Link>
-          </GridCol>
-        </GridRow>
-      </form>
+      <Details summary="Filter and sort activity" style={{fontSize: '100%'}}>
+        <form>
+          <GridRow>
+            <GridCol>Show me</GridCol>
+            <GridCol>
+              <Checkbox>My activity</Checkbox>
+              <Checkbox>My team's activity</Checkbox>
+            </GridCol>
+            <GridCol>
+              <Label>Activity types</Label>
+              <Select>
+                <option>All activity</option>
+              </Select>
+            </GridCol>
+            <GridCol>
+              <Link href="#">All filters</Link>
+            </GridCol>
+          </GridRow>
+        </form>
+      </Details>
     )
   }
 }
