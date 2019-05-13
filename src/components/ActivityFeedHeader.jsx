f2fabbd91757846eab507f239b5d36acb27dfae4
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-05-13 17:00:41 +0100

    Replace inline CSS with Styled Components

--- src/components/ActivityFeedHeader.jsx
+++ src/components/ActivityFeedHeader.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { H2, Button } from 'govuk-react'
+import styled from 'styled-components'
 
 import ActivityFeedFilters from './ActivityFeedFilters'
+
+const HeaderSummary = styled.div`
+  display: flex;
+  border-bottom: 2px solid #000;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+`;
+
+const HeaderCount = styled.div`
+  flex-grow: 1;
+  margin-top: 4px;
+  & > H2 {
+    font-weight: normal;
+    font-size: 28px;
+    margin-bottom: 0;
+  }
+`;
+
+const HeaderActions = styled.div`
+  flex-grow: 1;
+  text-align: right;
+  & > Button {
+    margin-bottom: 0;
+  }
+`;
 
 export default class ActivityFeedHeader extends React.Component {
   static propTypes = {
@@ -14,14 +40,14 @@ export default class ActivityFeedHeader extends React.Component {
 
     return (
       <React.Fragment>
-        <div style={{ display: 'flex', borderBottom: '2px solid #000', marginBottom: '10px', paddingBottom: '10px' }}>
-          <div style={{ flexGrow: 1, marginTop: '4px' }}>
-            <H2 style={{ fontWeight: 'normal', fontSize: '28px', marginBottom: 0 }}>{totalCards} activities</H2>
-          </div>
-          <div style={{ flexGrow: 1, textAlign: 'right' }}>
-            <Button buttonColour="#dee0e2" buttonTextColour="#000" style={{ marginBottom: 0 }}>Add an interaction</Button>
-          </div>
-        </div>
+        <HeaderSummary>
+          <HeaderCount>
+            <H2>{totalCards} activities</H2>
+          </HeaderCount>
+          <HeaderActions>
+            <Button buttonColour="#dee0e2" buttonTextColour="#000">Add an interaction</Button>
+          </HeaderActions>
+        </HeaderSummary>
         <ActivityFeedFilters />
       </React.Fragment>
     )
