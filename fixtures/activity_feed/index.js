a8fb52583cd75c1b8b4b68d6a99edce0e4483b44
Rafal Enden <git@enden.com>
2019-05-13 15:28:59 +0100

    Support multiple types of activities in the ActivityFeed component, the first one being an interaction

--- fixtures/activity_feed/index.js
+++ fixtures/activity_feed/index.js
@@ -1,4 +1,7 @@
 module.exports = [
 	require('./interactions/interaction'),
 	require('./interactions/investment_project'),
+	require('./interactions/service_delivery'),
+	require('./investment_projects/field_update'),
+	require('./investment_projects/project_created'),
 ]
--- src/components/ActivityFeedCard.jsx
+++ src/components/ActivityFeedCard.jsx
@@ -1,44 +1,25 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, InsetText, Table, Paragraph, H3 } from 'govuk-react'
+import { Details, Link, Table, Paragraph, H3 } from 'govuk-react'
 import { filter, includes, get, map } from 'lodash'
 import moment from 'moment'
 
+const ACTIVITY_FEED_OBJECT_TYPE_INTERACTION = 'dit:Interaction'
+
 const getPeople = (activity, personSubType) => {
-  return map(filter(activity['object']['attributedTo'], ({ type }) => {
-      return includes(type, `dit:${personSubType}`)
-    }), ({ id, name, 'dit:emailAddress': emailAddress }) => {
-      return {
-        id,
-        name,
-        emailAddress,
-      }
-    })
+  return map(filter(activity['object']['attributedTo'], ({type}) => {
+    return includes(type, `dit:${personSubType}`)
+  }), ({id, name, 'dit:emailAddress': emailAddress}) => {
+    return {
+      id,
+      name,
+      emailAddress,
+    }
+  })
 }
 
-export default class ActivityFeedCard extends React.Component {
-  static propTypes = {
-    activity: PropTypes.object,
-  }
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      showMoreContent: false,
-    }
-
-    this.onMoreContentClick = this.onMoreContentClick.bind(this)
-  }
-
-  onMoreContentClick(event) {
-    event.preventDefault()
-
-    this.setState({ showMoreContent: !this.state.showMoreContent })
-  }
-
+class ActivityFeedInteraction extends React.Component {
   render() {
-    const { activity } = this.props
+    const {activity} = this.props
     const published = moment(activity.published).fromNow()
     const advisers = getPeople(activity, 'Adviser')
     const subject = get(activity, 'object.dit:subject')
@@ -46,51 +27,58 @@ export default class ActivityFeedCard extends React.Component {
     const url = get(activity, 'object.url')
 
     return (
-      <div style={{ border: '1px solid #c0c0c0', padding: '10px' }}>
-        <div style={{ display: 'flex', flexFlow: 'row wrap' }}>
-          <div style={{ flexGrow: 1, marginBottom: '10px' }}>
+      <div style={{border: '1px solid #c0c0c0', padding: '10px'}}>
+        <div style={{display: 'flex', flexFlow: 'row wrap'}}>
+          <div style={{flexGrow: 1, marginBottom: '10px'}}>
             <Paragraph>An interaction with the company took place</Paragraph>
           </div>
-          <div style={{ flexGrow: 1, marginBottom: '10px', textAlign: 'right' }}>
+          <div style={{flexGrow: 1, marginBottom: '10px', textAlign: 'right'}}>
             <Paragraph>{published}</Paragraph>
           </div>
-          <div style={{ width: '100%', marginBottom: '10px' }}>
-            <H3 style={{ fontWeight: 'normal', color: '#005ea5' }}>{subject}</H3>
+          <div style={{width: '100%', marginBottom: '10px'}}>
+            <H3 style={{fontWeight: 'normal', color: '#005ea5'}}>{subject}</H3>
           </div>
         </div>
-        <Link href="#" onClick={this.onMoreContentClick}>Who was involved</Link>
-        {
-          this.state.showMoreContent ? <div>
-            <InsetText style={{ backgroundColor: '#f8f8f8', marginTop: '10px' }}>
-              <Table>
-                <Table.Row>
-                  <Table.CellHeader style={{ fontWeight: 'normal', border: 0 }}>Advisers</Table.CellHeader>
-                  <Table.Cell style={{ border: 0 }}>
-                    {advisers.map(({ id, name, emailAddress }) => {
-                      return <React.Fragment key={id}>
-                          <span>
-                            {name} <Link href={"mailto:" + emailAddress}>{emailAddress}</Link>
-                          </span>
-                      </React.Fragment>
-                    })}
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.CellHeader style={{ fontWeight: 'normal', border: 0 }}>DIT team</Table.CellHeader>
-                  <Table.Cell style={{ border: 0 }}>
-                    team
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.CellHeader style={{ fontWeight: 'normal', border: 0 }}>Services</Table.CellHeader>
-                  <Table.Cell style={{ border: 0 }}>{service}</Table.Cell>
-                </Table.Row>
-              </Table>
-              <Link href={url}>Go to the interaction detail page</Link>
-            </InsetText>
-          </div> : null
-        }
+        <Details summary="Who was involved" style={{fontSize: '100%', marginBottom: 0}}>
+          <Table>
+            <Table.Row>
+              <Table.CellHeader style={{fontWeight: 'normal', border: 0}}>Advisers</Table.CellHeader>
+              <Table.Cell style={{border: 0}}>
+                {advisers.map(({id, name, emailAddress}) => {
+                  return <React.Fragment key={id}>
+                      <span>
+                        {name} <Link href={"mailto:" + emailAddress}>{emailAddress}</Link>
+                      </span>
+                  </React.Fragment>
+                })}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.CellHeader style={{fontWeight: 'normal', border: 0}}>DIT team</Table.CellHeader>
+              <Table.Cell style={{border: 0}}>
+                team
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.CellHeader style={{fontWeight: 'normal', border: 0}}>Services</Table.CellHeader>
+              <Table.Cell style={{border: 0}}>{service}</Table.Cell>
+            </Table.Row>
+          </Table>
+          <Link href={url}>Go to the interaction detail page</Link>
+        </Details>
       </div>
     )
+  }
+}
+
+export default class ActivityFeedCard extends React.Component {
+  render() {
+    const {activity} = this.props
+
+    if (includes(activity['object']['type'], ACTIVITY_FEED_OBJECT_TYPE_INTERACTION)) {
+      return <ActivityFeedInteraction activity={activity} />
+    }
+
+    return null
   }
 }
