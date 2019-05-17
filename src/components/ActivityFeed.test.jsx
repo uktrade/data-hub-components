5c9576677b220fee870d4d4eee0a2dd5195f5831
Rafal Enden <git@enden.com>
2019-05-17 10:55:27 +0100

    Update tests naming

--- src/components/ActivityFeed.test.jsx
+++ src/components/ActivityFeed.test.jsx
@@ -4,14 +4,14 @@ import renderer from 'react-test-renderer'
 import ActivityFeed from './ActivityFeed'
 import interactionActivityFixture from '../../fixtures/activity_feed/interactions/interaction'
 
-it('renders empty feed', () => {
+test('should render empty feed', () => {
   const tree = renderer
     .create(<ActivityFeed activities={[]}/>)
     .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
-it('renders interaction activity', () => {
+test('should render activity', () => {
   const tree = renderer
     .create(<ActivityFeed activities={[interactionActivityFixture]}/>)
     .toJSON()
--- src/components/ActivityFeedCard.test.jsx
+++ src/components/ActivityFeedCard.test.jsx
@@ -4,14 +4,14 @@ import renderer from 'react-test-renderer'
 import ActivityFeedCard from './ActivityFeedCard'
 import interactionActivityFixture from '../../fixtures/activity_feed/interactions/interaction'
 
-it('handles incomplete data', () => {
+test('should render null', () => {
   const tree = renderer
     .create(<ActivityFeedCard activity={{}}/>)
     .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
-it('renders interaction activity', () => {
+test('should render interaction activity', () => {
   const tree = renderer
     .create(<ActivityFeedCard activity={interactionActivityFixture}/>)
     .toJSON()
