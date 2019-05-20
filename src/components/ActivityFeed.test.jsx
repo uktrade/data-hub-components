b1ff55c3925ec64bca35615b8822b7f90bae7ee5
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-05-20 14:22:22 +0100

    Update tests with describe

--- src/components/ActivityFeed.test.jsx
+++ src/components/ActivityFeed.test.jsx
@@ -4,16 +4,26 @@ import renderer from 'react-test-renderer'
 import ActivityFeed from './ActivityFeed'
 import interactionActivityFixture from '../../fixtures/activity_feed/interactions/interaction'
 
-test('should render empty feed', () => {
-  const tree = renderer
-    .create(<ActivityFeed activities={[]}/>)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+describe('ActivityFeed', () => {
+  describe('when there are no activities', () => {
+    test('should render empty feed', () => {
+      const tree = renderer
+        .create(<ActivityFeed activities={[]}/>)
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+
+  describe('when there are activities', () => {
+    test('should render activity', () => {
+      const tree = renderer
+        .create(<ActivityFeed activities={[interactionActivityFixture]}/>)
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
 })
 
-test('should render activity', () => {
-  const tree = renderer
-    .create(<ActivityFeed activities={[interactionActivityFixture]}/>)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
-})
+
+
+
--- src/components/ActivityFeedCard.test.jsx
+++ src/components/ActivityFeedCard.test.jsx
@@ -4,16 +4,26 @@ import renderer from 'react-test-renderer'
 import ActivityFeedCard from './ActivityFeedCard'
 import interactionActivityFixture from '../../fixtures/activity_feed/interactions/interaction'
 
-test('should render null', () => {
-  const tree = renderer
-    .create(<ActivityFeedCard activity={{}}/>)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+describe('ActivityFeedCard', () => {
+  describe('when the interaction is empty', () => {
+    test('should render null', () => {
+      const tree = renderer
+        .create(<ActivityFeedCard activity={{}}/>)
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+
+  describe('when there is an interaction', () => {
+    test('should render interaction activity', () => {
+      const tree = renderer
+        .create(<ActivityFeedCard activity={interactionActivityFixture}/>)
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
 })
 
-test('should render interaction activity', () => {
-  const tree = renderer
-    .create(<ActivityFeedCard activity={interactionActivityFixture}/>)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
-})
+
+
+
