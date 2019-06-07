695b5477084cf9e2909963528196526c89583572
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-06-07 15:57:16 +0100

    Add status badge to card header

--- src/activity-feed/activity-feed-cards/Interaction.jsx
+++ src/activity-feed/activity-feed-cards/Interaction.jsx
@@ -39,6 +39,11 @@ const CardHeaderDate = styled('div')`
   text-align: right;
 `
 
+const CardHeaderStatus = styled('div')`
+  width: 100%;
+  padding: ${SPACING.SCALE_2} 0 ${SPACING.SCALE_2};
+`
+
 const CardHeaderSubject = styled('div')`
   width: 100%;
   
@@ -52,6 +57,12 @@ const CardHeaderSubject = styled('div')`
 const CardDetails = styled(Details)`
   font-size: 100%;
   margin-bottom: 0;
+`
+
+const Badge = styled('span')`
+  border: 2px solid #c0c0c0;
+  border-radius: 4px;
+  padding: 2px 4px;
 `
 
 const getPeople = (activity, personSubType) => {
@@ -74,6 +85,20 @@ const getDescription = (types) => {
   return 'An interaction with the company took place'
 }
 
+const INTERACTION_STATUS = {
+  draft: {
+    UPCOMING: 'Upcoming interaction',
+    INCOMPLETE: 'Incomplete interaction',
+  }
+}
+
+const getStatus = (activity, isUpcoming) => {
+  const status = get(activity, 'object.dit:status')
+  if (INTERACTION_STATUS[status]) {
+    return isUpcoming ? INTERACTION_STATUS[status].UPCOMING : INTERACTION_STATUS[status].INCOMPLETE
+  }
+}
+
 export default class Interaction extends React.Component {
   static canRender(activity) {
     const types = get(activity, 'object.type')
@@ -84,10 +109,15 @@ export default class Interaction extends React.Component {
   }
 
   render() {
-    const {activity} = this.props
+    const { activity } = this.props
     const types = get(activity, 'object.type')
     const description = getDescription(types)
-    const startTime = moment(get(activity, 'object.startTime')).fromNow()
+    const startTime = get(activity, 'object.startTime')
+    const isUpcoming = new Date(startTime) > new Date()
+
+    const status = getStatus(activity, isUpcoming)
+    const cardHeaderStatus = status ? <CardHeaderStatus><Badge>{status}</Badge></CardHeaderStatus> : null
+    const cardHeaderStartTime = moment(startTime).fromNow()
     const advisers = getPeople(activity, 'Adviser')
     const subject = get(activity, 'object.dit:subject')
     const service = get(activity, 'object.dit:service.name')
@@ -100,8 +130,9 @@ export default class Interaction extends React.Component {
             {description}
           </CardHeaderDescription>
           <CardHeaderDate>
-            {startTime}
+            {cardHeaderStartTime}
           </CardHeaderDate>
+          {cardHeaderStatus}
           <CardHeaderSubject>
             <H3>{subject}</H3>
           </CardHeaderSubject>
