f174decb097cfd96064015112a26cf81686e07a5
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-06-13 17:22:38 +0100

    Update interaction card layout
    To match the design the card layout needs
    changing.

--- src/activity-feed/activity-feed-cards/Interaction.jsx
+++ src/activity-feed/activity-feed-cards/Interaction.jsx
@@ -4,46 +4,52 @@ import {
   filter,
   get,
   includes,
+  isEmpty,
   map,
   some,
-  isEmpty,
 } from 'lodash'
-import { Details, H3, Link, Table } from 'govuk-react'
+import {
+  Details,
+  H3,
+  Link,
+  Table,
+} from 'govuk-react'
 import styled from 'styled-components'
 import { SPACING } from '@govuk-react/constants'
 import PropTypes from 'prop-types'
 
 const Card = styled('div')`
-  border: ${props => props.isUpcoming ? '1px dashed #c0c0c0' : '1px solid #c0c0c0'};
+  border: ${({ isUpcoming }) => isUpcoming ? '1px dashed #c0c0c0' : '1px solid #c0c0c0'};
   padding: ${SPACING.SCALE_3};
-`
-
-const CardHeader = styled('div')`
   display: flex;
   flex-flow: row wrap;
-  
-  & > div {
-    margin-bottom: ${SPACING.SCALE_1};
-  }
 `
 
-const CardHeaderSubject = styled('div')`
+const CardContent = styled('div')`
   flex: 50%;
   
   & > H3 {
     font-weight: normal;
     color: #005ea5;
     margin-bottom: ${SPACING.SCALE_2};
+    
+    & > a:link, a:visited, a:hover, a:active {
+      text-decoration: none;
+    }
   }
-`
-
-const CardHeaderBadges = styled('div')`
-  text-align: right;
 `
 
 const CardDetails = styled(Details)`
   font-size: 100%;
-  margin-bottom: 0;
+  margin: ${SPACING.SCALE_3} 0 0;
+`
+
+const CardMeta = styled('div')`
+  text-align: right;
+`
+
+const CardBadges = styled('div')`
+  padding: ${SPACING.SCALE_2} 0 ${SPACING.SCALE_2};
 `
 
 const Badge = styled('span')`
@@ -127,25 +133,23 @@ export default class Interaction extends React.Component {
   render() {
     const { activity } = this.props
 
+    const url = get(activity, 'object.url')
+    const subject = get(activity, 'object.dit:subject')
+    const service = get(activity, 'object.dit:service.name')
     const startTime = get(activity, 'object.startTime')
     const cardHeaderStartTime = moment(startTime).fromNow()
-
     const status = getStatus(activity, startTime)
     const badgeLabel =  BADGE_LABELS[status.toUpperCase()]
 
     const isUpcoming = status === STATUS.UPCOMING
     const contacts = getPeople(activity, 'Contact')
-    const advisers = getPeople(activity, 'Adviser')
-    const subject = get(activity, 'object.dit:subject')
-    const service = get(activity, 'object.dit:service.name')
-    const url = get(activity, 'object.url')
-
     const contactsList = contacts.map(({id, name, emailAddress}) => (
       <span key={id}>
         {name} <Link href={"mailto:" + emailAddress}>{emailAddress}</Link>
       </span>
     ))
 
+    const advisers = getPeople(activity, 'Adviser')
     const advisersList = advisers.map(({id, name, emailAddress}) => (
       <span key={id}>
         {name} <Link href={"mailto:" + emailAddress}>{emailAddress}</Link>
@@ -153,24 +157,26 @@ export default class Interaction extends React.Component {
     ))
 
     return (
-      <Card isUpcoming={isUpcoming}>
-        <CardHeader>
-          <CardHeaderSubject>
-            <H3>{subject}</H3>
-          </CardHeaderSubject>
-          <CardHeaderBadges>
-            {cardHeaderStartTime}
-            <div style={{ padding: '10px 0 10px' }}><Badge>{badgeLabel}</Badge></div>
-          </CardHeaderBadges>
-        </CardHeader>
-        <CardDetails summary="Who was involved">
-          <Table>
-            <DetailsRow header="Contact(s)">{contactsList}</DetailsRow>
-            <DetailsRow header="Adviser(s)">{advisersList}</DetailsRow>
-            <DetailsRow header="Services">{service}</DetailsRow>
-          </Table>
-          <Link href={url}>Go to the interaction detail page</Link>
-        </CardDetails>
+      <Card isUpcoming={status === STATUS.UPCOMING}>
+        <CardContent>
+          <H3>
+            <Link href={url}>{subject}</Link>
+          </H3>
+          <CardDetails summary="Who was involved">
+            <Table>
+              <DetailsRow header="Contact(s)">{contactsList}</DetailsRow>
+              <DetailsRow header="Adviser(s)">{advisersList}</DetailsRow>
+              <DetailsRow header="Services">{service}</DetailsRow>
+            </Table>
+            <Link href={url}>Go to the interaction detail page</Link>
+          </CardDetails>
+        </CardContent>
+        <CardMeta>
+          {cardHeaderStartTime}
+          <CardBadges>
+            <Badge>{badgeLabel}</Badge>
+          </CardBadges>
+        </CardMeta>
       </Card>
     )
   }
