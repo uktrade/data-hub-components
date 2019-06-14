5e193e0c5769d8841d84ee8842683cbe29caf678
Paul Gain <paul.gain@digital.trade.gov.uk>
2019-06-14 18:09:14 +0100

    Add currency formatter
    Adding a currecy formatter that formats a value by prefixing it with a
    pound sign (£) and adds commas.

--- src/utils/NumberUtils.test.js
+++ src/utils/NumberUtils.test.js
@@ -6,35 +6,8 @@ describe('NumberUtils.js', () => {
       expect(NumberUtils.decimal()).toEqual(null)
     })
 
-    test('should not add a comma to tens', () => {
-      expect(NumberUtils.decimal(10)).toEqual('10')
-    })
-
-    test('should not add a comma to hundreds', () => {
-      expect(NumberUtils.decimal(100)).toEqual('100')
-    })
-
-    test('should add a single comma to thousands', () => {
-      expect(NumberUtils.decimal(1000)).toEqual('1,000')
-    })
-
-    test('should add a single comma to hundreds of thousands', () => {
-      expect(NumberUtils.decimal(100000)).toEqual('100,000')
-    })
-
     test('should add 2 commas to 1 million', () => {
       expect(NumberUtils.decimal(1000000)).toEqual('1,000,000')
-    })
-
-    test('should add 2 commas to 10 million', () => {
-      expect(NumberUtils.decimal(10000000)).toEqual('10,000,000')
-    })
-
-    test('should add 2 commas to 100 million', () => {
-      expect(NumberUtils.decimal(100000000)).toEqual('100,000,000')
-    })
-    test('should add 3 commas to 1 billion', () => {
-      expect(NumberUtils.decimal(1000000000)).toEqual('1,000,000,000')
     })
   })
 })
