3d1340e0568f5b963c458c4fa9cc682610b40784
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-06-21 13:48:57 +0100

    Remove default activity card

--- src/activity-feed/activity-feed-cards/index.js
+++ src/activity-feed/activity-feed-cards/index.js
@@ -1,11 +1,9 @@
 import InvestmentProject from './InvestmentProject'
 import Interaction from './Interaction'
 import Omis from './Omis'
-import Default from './Default'
 
 export default [
   Interaction,
   InvestmentProject,
   Omis,
-  Default,
 ]
