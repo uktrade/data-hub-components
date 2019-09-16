f66d24fc473a83db8f7e4ebe69a7473b2a147b69
Ian Leggett <contactleggett@gmail.com>
2019-09-16 13:09:28 +0100

    feat: Add tests around new form

---
+++ src/company-lists/__tests__/CreateListForm.test.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import CreateListForm from '../CreateListForm'
+
+describe('CreateListForm', () => {
+  describe('When passing props', () => {
+    const wrapper = mount(
+      <CreateListForm
+        onSubmitHandler={() => {}}
+        name="listName"
+        hint="Hint"
+        label="List name"
+        cancelUrl="/companies/"
+        maxLength={30}
+      />,
+    )
+    const input = wrapper.find('input')
+    const label = wrapper.find('label').at(0)
+    const hint = wrapper.find('span').at(0)
+    const button = wrapper.find('button')
+    const cancelUrl = wrapper.find('a[href="/companies/"]')
+    test('should render correctly', () => {
+      expect(label.text()).toEqual('List name')
+      expect(hint.text()).toEqual('Hint')
+      expect(input).toHaveLength(1)
+      expect(button).toHaveLength(1)
+      expect(cancelUrl).toHaveLength(1)
+    })
+  })
+  describe('When not passing props that are not required', () => {
+    const wrapper = mount(
+      <CreateListForm
+        onSubmitHandler={() => {}}
+        name="listName"
+        cancelUrl="/companies/"
+        label="List name"
+        maxLength={30}
+      />,
+    )
+    const hint = wrapper.find('span').at(0)
+    const cancelUrl = wrapper.find('a[href="/companies/"]')
+    test('should render correctly', () => {
+      expect(hint.text()).toEqual('')
+      expect(cancelUrl).toHaveLength(1)
+    })
+  })
+  describe('when submitting with no errors', () => {
+    const onSubmitSpy = jest.fn()
+    const wrapper = mount(
+      <CreateListForm
+        onSubmitHandler={onSubmitSpy}
+        name="listName"
+        cancelUrl="/companies/"
+        label="List name"
+        maxLength={30}
+      />,
+    )
+    test('should fire a onSubmit handler', () => {
+      const input = wrapper.find('input')
+      input.simulate('change', { target: { value: 'list name' } })
+      wrapper.simulate('submit')
+      expect(onSubmitSpy).toBeCalledTimes(1)
+    })
+  })
+  describe('when submitting form with errors', () => {
+    const onSubmitSpy = jest.fn()
+    const wrapper = mount(
+      <CreateListForm
+        onSubmitHandler={onSubmitSpy}
+        name="listName"
+        cancelUrl="/companies/"
+        label="List name"
+        maxLength={30}
+      />,
+    )
+    test('should prompt for a list name', () => {
+      wrapper.simulate('submit')
+      const errorMessage = wrapper.find('span').at(1)
+      expect(onSubmitSpy).toBeCalledTimes(0)
+      expect(errorMessage.text()).toContain('Enter a name for your list')
+    })
+    test('should ask for less than 30 characters', () => {
+      const input = wrapper.find('input')
+      input.simulate('change', { target: { value: 'oneverylooooooooooooooooooooooooooooooooooooonnnnnngggg name' } })
+      wrapper.simulate('submit')
+      const errorMessage = wrapper.find('span').at(1)
+      expect(onSubmitSpy).toBeCalledTimes(0)
+      expect(errorMessage.text()).toContain('Enter list name which is no longer than 30 characters')
+    })
+  })
+})
