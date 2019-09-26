bf809342af4486276b470973a2f0a8ea767160b3
David Matthews <david.matthews@digital.trade.gov.uk>
2019-09-26 10:55:09 +0100

    feat: add CollectionHeader component

---
+++ src/collection-list/CollectionHeader.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '@govuk-react/button'
+import styled from 'styled-components'
+import pluralise from 'pluralise'
+import { H2 } from '@govuk-react/heading'
+import { BLACK, GREY_3 } from 'govuk-colours'
+import { SPACING, MEDIA_QUERIES, FONT_SIZE } from '@govuk-react/constants'
+
+const StyledSummary = styled('div')`
+  display: flex;
+  flex-flow: row wrap;
+  border-bottom: ${SPACING.SCALE_1} solid ${BLACK};
+  margin-bottom: ${SPACING.SCALE_2};
+  padding-bottom: ${SPACING.SCALE_1};
+`
+
+const StyledCount = styled('div')`
+  margin-top: ${SPACING.SCALE_1};
+
+  & > H2 {
+    font-weight: normal;
+    font-size: ${FONT_SIZE.SIZE_27};
+    margin-bottom: 0;
+  }
+`
+
+const StyledActions = styled('div')`
+  text-align: right;
+  width: 100%;
+  margin: ${SPACING.SCALE_1} 0;
+
+  ${MEDIA_QUERIES.TABLET} {
+    width: 0;
+    flex-grow: 1;
+  }
+`
+
+const StyledLink = styled.a`
+  margin-bottom: 0;
+`
+
+function CollectionHeader({
+  totalItems,
+  itemName,
+  addItemUrl }) {
+  const headerText = pluralise.withCount(totalItems, `% ${itemName}`)
+
+  return (
+    <StyledSummary>
+      <StyledCount>
+        <H2>{headerText}</H2>
+      </StyledCount>
+      <StyledActions>
+        {addItemUrl && (
+          <Button
+            as={StyledLink}
+            href={addItemUrl}
+            buttonColour={GREY_3}
+            buttonTextColour={BLACK}
+          >
+            Add {itemName}
+          </Button>
+        )}
+      </StyledActions>
+    </StyledSummary>
+  )
+}
+
+CollectionHeader.propTypes = {
+  totalItems: PropTypes.number.isRequired,
+  itemName: PropTypes.string.isRequired,
+  addItemUrl: PropTypes.string,
+}
+
+CollectionHeader.defaultProps = {
+  addItemUrl: null,
+}
+
+export default CollectionHeader
---
+++ src/collection-list/__tests__/CollectionHeader.test.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import CollectionHeader from '../CollectionHeader'
+import capitalProfileHeading from '../__fixtures__/capitalProfileHeading'
+
+describe('CollectionHeader', () => {
+  let wrapper
+
+  describe('when props are passed', () => {
+    beforeAll(() => {
+      wrapper = mount(
+        <CollectionHeader
+          totalItems={capitalProfileHeading.totalItems}
+          itemName={capitalProfileHeading.itemName}
+          addItemText={capitalProfileHeading.addItemText}
+          addItemUrl={capitalProfileHeading.addItemUrl}
+        />,
+      )
+    })
+
+    test('should render the component', () => {
+      expect(wrapper.find(CollectionHeader)
+        .exists()).toBe(true)
+    })
+
+    test('should render the totalItems and itemName', () => {
+      expect(wrapper.find('h2')
+        .text()).toBe('1 profile')
+    })
+
+    test('should render the addItemText', () => {
+      expect(wrapper.find('a')
+        .text()).toBe('Add profile')
+    })
+
+    test('should render the addItemUrl', () => {
+      expect(wrapper.find("a[href='#']"))
+        .toHaveLength(1)
+    })
+  })
+
+  describe('when the totalItems is > 1 and no addItemText and addItemUrl are passed', () => {
+    beforeAll(() => {
+      wrapper = mount(
+        <CollectionHeader
+          totalItems={2}
+          itemName={capitalProfileHeading.itemName}
+        />,
+      )
+    })
+
+    test('should render the component', () => {
+      expect(wrapper.find(CollectionHeader)
+        .exists()).toBe(true)
+    })
+
+    test('should render the totalItems with the itemNamePlural', () => {
+      expect(wrapper.find('h2')
+        .text()).toBe('2 profiles')
+    })
+
+    test('should not render the addItem button', () => {
+      expect(wrapper.find('a')
+        .exists()).toBe(false)
+    })
+  })
+})
