5ba4ec9a7272101d5dd9c745a2b3c0f8e9c81ab2
David Matthews <david.matthews@digital.trade.gov.uk>
2019-10-01 17:36:08 +0100

    feat: Add CollectionDownload component

--- src/collection-list/__stories__/CollectionList.stories.jsx
+++ src/collection-list/__stories__/CollectionList.stories.jsx
@@ -8,6 +8,7 @@ import {
 } from '../__fixtures__'
 import CollectionItem from '../CollectionItem'
 import CollectionHeader from '../CollectionHeader'
+import CollectionDownload from '../CollectionDownload'
 
 storiesOf('Collection', module).add('Collection', () => (
   <>
@@ -17,14 +18,73 @@ storiesOf('Collection', module).add('Collection', () => (
       addItemText={capitalProfileHeading.addItemText}
       addItemUrl={capitalProfileHeading.addItemUrl}
     />
+    <CollectionDownload
+      totalItems={capitalProfileHeading.totalItems}
+      itemName={capitalProfileHeading.itemName}
+      downloadUrl={capitalProfileHeading.downloadUrl}
+    />
     <CollectionItem
       id={capitalProfileItem.id}
-      headingUrl={capitalProfileItem.headerUrl}
+      headingUrl={capitalProfileItem.headerLink}
       headingText={capitalProfileItem.headerText}
       badges={capitalProfileItem.badges}
       metadata={capitalProfileItem.metadata}
     />
   </>
+))
+
+storiesOf('Collection', module).add('Collection Header', () => (
+  <CollectionHeader
+    totalItems={capitalProfileHeading.totalItems}
+    itemName={capitalProfileHeading.itemName}
+    addItemText={capitalProfileHeading.addItemText}
+    addItemUrl={capitalProfileHeading.addItemUrl}
+  />
+))
+
+storiesOf('Collection', module).add('Collection Download - no items', () => (
+  <CollectionDownload
+    totalItems={0}
+    itemName={capitalProfileHeading.itemName}
+    downloadUrl={null}
+  />
+))
+
+storiesOf('Collection', module).add('Collection Download - 1 item', () => (
+  <CollectionDownload
+    totalItems={1}
+    itemName={capitalProfileHeading.itemName}
+    downloadUrl={null}
+  />
+))
+
+storiesOf('Collection', module).add('Collection Download - 101 items', () => (
+  <CollectionDownload
+    totalItems={101}
+    itemName={capitalProfileHeading.itemName}
+    downloadUrl={null}
+  />
+))
+
+storiesOf('Collection', module).add(
+  'Collection Download - need to filter',
+  () => (
+    <CollectionDownload
+      totalItems={5001}
+      itemName={capitalProfileHeading.itemName}
+      downloadUrl={capitalProfileHeading.downloadUrl}
+    />
+  )
+)
+
+storiesOf('Collection', module).add('Capital Profile item', () => (
+  <CollectionItem
+    id={capitalProfileItem.id}
+    headingUrl={capitalProfileItem.headerUrl}
+    headingText={capitalProfileItem.headerText}
+    badges={capitalProfileItem.badges}
+    metadata={capitalProfileItem.metadata}
+  />
 ))
 
 storiesOf('Collection', module).add('Collection Header', () => (
