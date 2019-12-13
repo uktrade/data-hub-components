f5f6648c230ea6ccac80578e8db29d6381dccb67
Paul Gain <paul@javexconsulting.com>
2019-12-13 12:27:25 +0000

    feat: Add updatedOn formatted date

--- src/utils/DateUtils.js
+++ src/utils/DateUtils.js
@@ -4,4 +4,8 @@ export default class DateUtils {
   static format(dateStr) {
     return moment(dateStr).format('DD MMM YYYY')
   }
+
+  static formatWithTime(dateTimeStr) {
+    return moment(dateTimeStr).format('D MMM YYYY, h:mma')
+  }
 }
