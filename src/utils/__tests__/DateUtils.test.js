e760f46a567819ec1cef3834bab9e8e826424fb1
Paul Gain <paul@javexconsulting.com>
2019-12-13 12:27:25 +0000

    feat: Add updatedOn formatted date

--- src/utils/__tests__/DateUtils.test.js
+++ src/utils/__tests__/DateUtils.test.js
@@ -12,4 +12,17 @@ describe('DateUtils.js', () => {
       expect(DateUtils.format('1973-10-01')).toEqual('01 Oct 1973')
     })
   })
+
+  describe('the formatWithTime() function', () => {
+    test("should correctly format today's date and time", () => {
+      const today = moment().format('D MMM YYYY, h:mma')
+      expect(DateUtils.formatWithTime()).toEqual(today)
+    })
+
+    test('should correctly format a given date and time', () => {
+      const dateAndTime = '2019-12-10T18:12:37.649476Z'
+      const formattedDateAndTime = DateUtils.formatWithTime(dateAndTime)
+      expect(formattedDateAndTime).toEqual('10 Dec 2019, 6:12pm')
+    })
+  })
 })
